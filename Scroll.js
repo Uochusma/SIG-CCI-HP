25c814580338d99f5d2541e01c8bd48dbb350a65
芳野魁 <YoshinoKai@barolo.itolab.nitech.ac.jp>
2017-03-13 13:30:58 +0900

    念のため

---
+++ Scroll.js
@@ -0,0 +1,10 @@
+//ページ内リンク、#非表示。スムーズスクロール
+  $(".scroll").click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var offset = $(this).attr("data-offset");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-50;//通常より50ピクセル上がスクロールの目的地
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
