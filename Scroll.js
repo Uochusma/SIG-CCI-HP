c028e17f77a98a41b56c4121a54e0a76cc0deef0
芳野魁 <YoshinoKai@barolo.itolab.nitech.ac.jp>
2017-04-14 19:39:36 +0900

    要望を修正した。

--- Scroll.js
+++ Scroll.js
@@ -4,7 +4,17 @@
     var href= $(this).attr("href");
     var offset = $(this).attr("data-offset");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top-60;//通常より60ピクセル上がスクロールの目的地
+    var windowWidth = window.innerWidth;
+    offset2 = 60
+    //console.log(windowWidth);
+    if(windowWidth >= 1560){
+      offset2 = 110;
+    }
+    else if(windowWidth >= 768){
+      offset2 = 160;
+    }
+
+    var position = target.offset().top-offset2;//通常より60ピクセル上がスクロールの目的地
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
